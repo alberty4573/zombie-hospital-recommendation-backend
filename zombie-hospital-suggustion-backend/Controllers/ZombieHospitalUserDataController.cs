5b65abcd344f5006c0a8ff9559a17f7c54fd4499
alberty4573 <shenlieku@gmail.com>
2021-03-27 16:46:44 +1100

    * ZombieHospitalUserDataController.cs: added CORS policy
    * UserData.cs:

---
+++ zombie-hospital-suggustion-backend/Controllers/ZombieHospitalUserDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using zombie_hospital_suggustion_backend.Models;
+
+namespace zombie_hospital_suggustion_backend.Controllers
+{
+    [ApiController]
+    [Route("api/userdata")]
+    public class ZombieHospitalUserDataController
+    {
+        [HttpPost]
+       public ActionResult <IEnumerable<UserData>> PostUserData(UserData userData)
+        {
+            var usr = "getfnskfn";
+            return null;
+        }
+
+        
+    }
+}
---
+++ zombie-hospital-suggustion-backend/Models/UserData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace zombie_hospital_suggustion_backend.Models
+{
+    public class UserData
+    {
+        public string illness { get; set; }
+        public string name { get; set; }
+    }
+}
